13584ffb1cb85ef0a273a1727f1ad633c00aae93
Rohan Kewalramani <45906368+rohankewal@users.noreply.github.com>
2024-06-05 20:38:04 -0400

    initial commit -> been working on rustlings for a few days now

---
+++ exercises/functions/functions1.rs
@@ -0,0 +1,14 @@
+// functions1.rs
+//
+// Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
+// hint.
+
+// DONE
+
+fn main() {
+    call_me();
+}
+
+fn call_me() {
+
+}
---
+++ exercises/variables/variables3.rs
@@ -0,0 +1,11 @@
+// variables3.rs
+//
+// Execute `rustlings hint variables3` or use the `hint` watch subcommand for a
+// hint.
+
+// DONE
+
+fn main() {
+    let x = 32;
+    println!("Number {}", x);
+}
